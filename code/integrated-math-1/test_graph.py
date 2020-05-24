1ad7c37e8f413f578f843f483bf66bf72e19af97
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-05-23 17:27:46 -0700

    Extended the range, and added line colors

--- code/integrated-math-1/test_graph.py
+++ code/integrated-math-1/test_graph.py
@@ -17,9 +17,9 @@ for direction in ["left", "right", "bottom", "top"]:
     # hides borders
     ax.axis[direction].set_visible(False)
     
-x = np.arange(-7, 7)
-ax.plot(x, 2 - x)
-ax.plot(x, 4*x - 8)
+x = np.arange(-10, 10)
+ax.plot(x, 2 - x, '-g')
+ax.plot(x, 4*x - 8, '-c')
 
 plt.legend(['x + y = 2', '4x - y = 8'])
 plt.xlim(-9, 9)
